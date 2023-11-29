fc5b2cf52fe22b24c55a3ed81cd1280e3b4b9532
shar0507 <sharwari.deshmukh211@vit.edu>
2023-11-29 08:18:05 +0530

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+session_save_path('C:\xampp\htdocs\limit');
+session_start();
+
+$maxSessions = 3; 
+
+if (!isset($_SESSION['session_count'])) {
+    $_SESSION['session_count'] = 1;
+} else {
+    $_SESSION['session_count']++;
+
+    if ($_SESSION['session_count'] > $maxSessions) {
+        session_unset();
+        session_destroy();
+        echo "Maximum session limit exceeded. Please log in again.";
+        exit;
+    }
+}
+echo "Session active. Session count: " . $_SESSION['session_count'];
+
+?>
